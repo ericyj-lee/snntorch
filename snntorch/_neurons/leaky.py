577e29a9b64d3de7873dd47662e89058f141b34d
Eric Yeu-Jer Lee <75584302+ericyj-lee@users.noreply.github.com>
2024-02-18 10:11:07 -0500

    Update leaky.py

--- snntorch/_neurons/leaky.py
+++ snntorch/_neurons/leaky.py
@@ -37,6 +37,34 @@ class Leaky(LIF):
             reset_delay,
         )
 
+    self._init_mem()
+        self.init_hidden = init_hidden
+
+        if self.reset_mechanism_val == 0:  # reset by subtraction
+            self.state_function = self._base_sub
+        elif self.reset_mechanism_val == 1:  # reset to zero
+            self.state_function = self._base_zero
+        elif self.reset_mechanism_val == 2:  # no reset, pure integration
+            self.state_function = self._base_int
+        
+        self.reset_delay = reset_delay
+
+        if not self.reset_delay and self.init_hidden:
+            raise NotImplementedError("`reset_delay=True` is only supported for `init_hidden=False`")
+
+
+    def _init_mem(self):
+        mem = torch.zeros(1)
+        self.register_buffer("mem", mem)
+
+    def reset_mem(self):
+        self.mem = torch.zeros_like(self.mem, device=self.mem.device)
+
+    def init_leaky(self):
+        """Deprecated, use :class:`Leaky.reset_mem` instead"""
+        self.reset_mem()
+        return self.mem
+
     def forward(self, input_, mem=None):
         if not mem == None:
             self.mem = mem
@@ -78,3 +106,40 @@ class Leaky(LIF):
             return spk
         else:
             return spk, self.mem
+
+    def _base_state_function(self, input_):
+        base_fn = self.beta.clamp(0, 1) * self.mem + input_
+        return base_fn
+
+    def _base_sub(self, input_):
+        return self._base_state_function(input_) - self.reset * self.threshold
+
+    def _base_zero(self, input_):
+        self.mem = (1 - self.reset) * self.mem
+        return self._base_state_function(input_)
+
+    def _base_int(self, input_):
+        return self._base_state_function(input_)
+
+    @classmethod
+    def detach_hidden(cls):
+        """Returns the hidden states, detached from the current graph.
+        Intended for use in truncated backpropagation through time where
+        hidden state variables are instance variables."""
+
+        for layer in range(len(cls.instances)):
+            if isinstance(cls.instances[layer], Leaky):
+                cls.instances[layer].mem.detach_()
+
+    @classmethod
+    def reset_hidden(cls):
+        """Used to clear hidden state variables to zero.
+        Intended for use where hidden state variables are instance variables.
+        Assumes hidden states have a batch dimension already."""
+        for layer in range(len(cls.instances)):
+            if isinstance(cls.instances[layer], Leaky):
+                cls.instances[layer].mem = torch.zeros_like(
+                    cls.instances[layer].mem,
+                    device=cls.instances[layer].mem.device,
+                )
+
